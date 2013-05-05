237a76a007fe9a744ca1b818a9ebf20e364ad752
Ayesha Bose <aybose@mit.edu>
2013-05-05 15:52:53 -0400

    Added testing strategy to gui, specified response types in Client

--- src/main/ChatGUI.java
+++ src/main/ChatGUI.java
@@ -6,6 +6,83 @@ import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import javax.swing.*;
 
+/**
+ * Testing Strategy:
+ *
+ * Connect to Chat Server
+ * Create a username
+ * Create a new room
+ * Type message into room
+ * Leave room
+ * ---
+ * This test should show the message displayed next to the specified username, the new chat room should be in the list
+ * of available chat rooms. The user should then be able to leave the room.
+ *
+ * Connect to Chat Server
+ * Create a username
+ * Create a new room
+ * Type message into room
+ * Leaves room
+ * Other user connects to chat server
+ * Other user creates a username
+ * Other user creates a new room
+ * Other user types message into new room
+ * ---
+ * This test should show the message displayed next to the first specified username, the new chat room should be in the list
+ * of available chat rooms, the other user should be able to create a new username and login to the chat server and not see the 
+ * other user's chat room in the list, the new chat room that the second user creates should join the list of available chat
+ * rooms, the message the second user types should be only displayed in the second user's chat room next to the second user's
+ * name.
+ *
+ * Connect to Chat Server
+ * Create a username
+ * Create a new room
+ * Type message into room
+ * Other user connects to chat server
+ * Other user creates a username
+ * Other user joins existing room
+ * Other user types message into new room
+ * First user types message into room
+ * First user leaves room
+ * Second user leaves room
+ * ---
+ * This test should show the message displayed next to the first specified username, the new chat room should be in the list
+ * of available chat rooms, the other user should be able to create a new username and login to the chat server and see the 
+ * other user's chat room in the list. The second user should be able to join the first user's room. The first user should be
+ * notified that the second user joined. The second user should not be able to see the first user's previous message. The 
+ * second user should be able to type in a message, which will be displayed next to their username. The first user should
+ * see this new message. The first user should be able to type in a message, which will be displayed next to their username.
+ * The second user should see this new message. The second user should be notified that the first user left. The second user
+ * should be able to leave.
+ * 
+ * Connect to Chat Server
+ * Create a username
+ * Create a new room
+ * Type message into room
+ * Other user connects to chat server
+ * Other user creates a username
+ * Other user joins existing room
+ * Other user types delayed message into new room
+ * First user types message into room
+ * First user leaves room
+ * Second user leaves room
+ * ---
+ * This test should show the message displayed next to the first specified username, the new chat room should be in the list
+ * of available chat rooms, the other user should be able to create a new username and login to the chat server and see the 
+ * other user's chat room in the list. The second user should be able to join the first user's room. The first user should be
+ * notified that the second user joined. The second user should not be able to see the first user's previous message. The 
+ * second user should be able to type in a delayed message, which will be displayed next to their username. The first user
+ * should be able to type in a message before the second user's message shows up, which will be displayed next to the first 
+ * username. The second user's delayed message should show up before the first user's message. Both users should be able to 
+ * see both messages.The second user should be notified that the first user left. The second user should be able to leave.
+ * 
+ * Other tests:
+ * One user, multiple chat rooms
+ * Multiple users, one chat room
+ * Multiple users, multiple chat rooms
+ * 
+ */
+
 public class ChatGUI extends JFrame {
 
     //GUI variable declarations: DO NOT TOUCH
@@ -17,7 +94,12 @@ public class ChatGUI extends JFrame {
     private JMenu fileMenu, helpMenu;
     private JMenuItem jMenuItem1, jMenuItem2, jMenuItem3, jMenuItem4, jMenuItem5, jMenuItem6;
     //END OF VARIABLE DECLARATION
-
+    
+    /**
+     * ChatGUI constructor
+     * 
+     * calls initComponents() to create gui
+     */
     public ChatGUI() {
         initComponents();
     }
@@ -168,8 +250,6 @@ public class ChatGUI extends JFrame {
     }
 
     private void sendButtonHandler(ActionEvent e) {
-        //eventually move this to a separate method
-        //to make things more modular
         String nothing = "";
         String username = "jholliman"; //temporary var for testing
         String messageText = inputTextArea.getText();
