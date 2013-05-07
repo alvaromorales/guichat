2f0608ee4def8e8d51ade351157a3cf4dd6bb769
jholliman <jholliman1@gmail.com>
2013-05-06 20:11:00 -0400

    Removed unused imports

--- src/main/ChatGUI.java
+++ src/main/ChatGUI.java
@@ -3,15 +3,10 @@ package main;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.AdjustmentEvent;
-import java.awt.event.AdjustmentListener;
 import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import com.google.gson.*;
-
-
 import javax.swing.*;
 
 /**
